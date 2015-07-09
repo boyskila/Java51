f90561adb9c8cb1afad36b6fc711f4073a6dcdf7
Boyko <bmlalov@abv.bg>
2015-07-09 16:34:47 +0300

    optimize gasStation solution

--- src/com/hackbulgaria51/week5/gasstations/GPS.java
+++ src/com/hackbulgaria51/week5/gasstations/GPS.java
@@ -6,15 +6,13 @@ import java.util.Vector;
 public class GPS {
 	public static Vector<Integer> getGasStations(int tripDistance,
 			int tankSize, Vector<Integer> gasStations) {
-
 		Vector<Integer> result = new Vector<Integer>();
 		int temp = tankSize;
 		for (int i = 0; i < gasStations.size(); i++) {
-			if (temp > gasStations.get(i)) {
-				continue;
-			} else {
-				result.add(gasStations.get(i - 1));
-				temp = tankSize + gasStations.get(i - 1);
+			if (temp < gasStations.get(i)) {
+				int station = gasStations.get(i - 1);
+				result.add(station);
+				temp = tankSize + station;
 			}
 		}
 		if (temp < tripDistance) {
@@ -27,20 +25,15 @@ public class GPS {
 		Scanner scanner = new Scanner(System.in);
 		int tripDistance = scanner.nextInt();
 		int tankSize = scanner.nextInt();
-
 		int gasStationsCount = scanner.nextInt();
 		Vector<Integer> gasStations = new Vector<Integer>();
-
 		for (int i = 0; i < gasStationsCount; i++) {
 			gasStations.add(scanner.nextInt());
 		}
-
 		Vector<Integer> result = getGasStations(tripDistance, tankSize,
 				gasStations);
-
 		for (int i = 0; i < result.size(); i++) {
 			System.out.println(result.get(i));
 		}
-
 	}
 }
